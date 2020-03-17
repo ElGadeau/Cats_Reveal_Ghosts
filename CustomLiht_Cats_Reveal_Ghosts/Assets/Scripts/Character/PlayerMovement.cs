54b68c2772fa8d320c50e3a4e8a24c01f27529fa
ElGadeau <l.gadeau@student.isartdigital.com>
2020-03-17 15:40:29 -0400

    added some script to move the character and the camera and created a base scene
    to use the scene, you need to import the 3d models from the drive. we wont push it to the git because they are not the final version

---
+++ CustomLiht_Cats_Reveal_Ghosts/Assets/Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Control Settings")]
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float mouseSensitivity = 1.0f;
+    
+    [Header("Controlled Objects")]
+    [SerializeField] private Camera playerCamera = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playerCamera == null)
+        {
+            Debug.Log("Camera is not set");
+            Debug.Break();
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #elif UNITY_WEBPLAYER
+            Application.OpenURL(webplayerQuitURL);
+            #else
+            Application.Quit();
+            #endif
+        }
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerControl();
+        CameraControl();
+    }
+
+    void PlayerControl()
+    {
+        float ver = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+        
+        Vector3 forward = transform.forward * ver;
+        Vector3 right = transform.right * hor;
+        
+        Vector3 mov = forward + right;
+
+        mov *= speed;
+        mov *= Time.deltaTime;
+        gameObject.GetComponent<Rigidbody>().MovePosition(transform.position + mov);
+    }
+
+    void CameraControl()
+    {
+        float rotX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float rotY = -Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        playerCamera.transform.Rotate(rotY, 0, 0);
+        transform.Rotate(0, rotX, 0); 
+    }
+}
