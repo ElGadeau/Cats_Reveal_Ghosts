e237cbc42d593127aa311670997581b1f3709bd5
Lilian <l.gadeau@student.isartdigital.com>
2020-04-03 11:06:52 -0400

    the game now have sound when playing and less cats in games

---
+++ CustomLiht_Cats_Reveal_Ghosts/Assets/Scripts/WorldManagement/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip sound = null;
+    [SerializeField] private AudioClip secondSound = null;
+    private AudioSource usedSound = new AudioSource();
+
+    private void Awake()
+    {
+        if (secondSound == null)
+        {
+            usedSound = gameObject.AddComponent<AudioSource>();
+            usedSound.clip = sound;
+        }
+        else
+        {
+            int rng = Random.Range(0, 100);
+            if (rng < 2)
+            {
+                usedSound = gameObject.AddComponent<AudioSource>() as AudioSource;
+                usedSound.clip = secondSound;
+            }
+            else
+            {
+                usedSound = gameObject.AddComponent<AudioSource>();
+                usedSound.clip = sound;
+            }
+        }
+        
+        usedSound.loop = true;
+        usedSound.volume = 0.15f;
+        PlaySound();
+    }
+
+    private void PlaySound()
+    {
+        if (usedSound == null)
+        {
+            Debug.Log("WHat is happeneing here");
+            return;
+        }
+        usedSound.Play();
+    }    
+}
